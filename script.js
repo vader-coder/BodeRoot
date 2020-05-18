a52d6e55d681bb0d02d02aec63d88cde58755d51
Patrick Wheeler <pwheele1@swarthmore.edu>
2020-05-18 18:10:23 -0400

    nerdamer for finding coeffcients

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ function onInput() {
   const code1 = node1.compile();
   var num = code1.evaluate(); // 5
   console.log(num);
+
+  var factors = '(1+3x)(1+x)(1+2x)';
+  console.log('original factors: ' + factors);
+  var y = nerdamer('expand(' + factors + ')');
+  var polynomialform = y.toString();
+  console.log('polynomial form: ' + polynomialform);
+  var coef = polynomialform.split('+').map(v=>v.trim()).map(v=>v.split('x')[0]).map(v=>v.replace(/^\*+|\*+$/g, ''));
+  console.log('coeficients: ' + coef);
   /*for (let i=0; i<eq.length; i++) {
   }*/
 }
