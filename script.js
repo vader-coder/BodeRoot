f7116c533fa210d1ad1391b462912cbe611bd61a
Patrick Wheeler <pwheele1@swarthmore.edu>
2020-05-18 17:50:43 -0400

    used math.js, have input field

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+var polyInput = [];
 
+function onInput() {
+  var terms = parseInt(document.getElementById('terms').value);
+  console.log(terms);
+  var eq = document.getElementById('equation').value;
+  const node1 = math.parse('sqrt(5^2 - 4^2)');
+  const code1 = node1.compile();
+  var num = code1.evaluate(); // 5
+  console.log(num);
+  /*for (let i=0; i<eq.length; i++) {
+  }*/
+}
